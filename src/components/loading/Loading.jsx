cd3aebc7d612c849ab39f698789859bd18ab7342
MiltonEmm008 <merp2067@gmail.com>
2025-07-01 14:57:11 -0700

    modificacion

--- src/components/loading/Loading.jsx
+++ src/components/loading/Loading.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Loading() {
   return (
     <div>
-        <h3 className='text-gray-500 font-semibold italic'>Loading, please wait...</h3>
+        <h3 className='text-gray-500 font-semibold italic'>Loading recipes, please wait...</h3>
     </div>
   )
 }
