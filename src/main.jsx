565ccad0c0da95869fc32a62f658103f0f1bb609
MiltonEmm008 <merp2067@gmail.com>
2025-07-01 15:33:05 -0700

    hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-// import { HashRouter } from "react-router-dom";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import GlobalState from "./context/GlobalState.jsx";
 
 //Use hashrouter
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <StrictMode>
       <GlobalState>
         <App />
       </GlobalState>
     </StrictMode>
-  </BrowserRouter>
+  </HashRouter>
 );
